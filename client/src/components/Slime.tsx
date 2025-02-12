0f8e6ab4a8f48bb7332e635c679e7bf42325debb
Foxrim <Flavien085@gmail.com>
2025-02-12 21:31:01 +0100

    [fix] - code mort supp

--- client/src/components/Slime.tsx
+++ client/src/components/Slime.tsx
@@ -1,17 +1,12 @@
-import { useEffect } from "react";
 import { useSlime } from "../contexts/SlimeContext";
 import styles from "../styles/Game.module.css";
 
 export default function Slime() {
-  const { slime, fetchSlime } = useSlime();
-
-  useEffect(() => {
-    fetchSlime();
-  }, [fetchSlime]);
+  const { slime } = useSlime();
 
   return (
     <div
-      className={`${styles.slime} ${slime?.color === "red" ? styles.red : slime?.color === "blue" ? styles.blue : slime?.color === "green" ? styles.green : styles.grey}   `}
+      className={`${styles.slime} ${slime?.color === "red" ? styles.red : slime?.color === "blue" ? styles.blue : slime?.color === "green" ? styles.green : styles.grey}`}
     >
       ok
     </div>
